e7777c77a7e0eebd67adc140710bed68e9a46967
= <theanh24013@gmail.com>
2024-06-22 18:50:58 +0700

    add detection model and code

--- api/views.py
+++ api/views.py
@@ -2,29 +2,91 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import ImageUploadSerializer
-from django.core.files.uploadedfile import InMemoryUploadedFile
 import numpy as np
 from django.http import HttpResponse
 import io
 from PIL import Image
 from model.detector import ImageManipulationDetector
+from model.UNet import UNet
 
 class DetectRegionsView(APIView):
     def post(self, request, *args, **kwargs):
         serializer = ImageUploadSerializer(data=request.data)
-        detector = ImageManipulationDetector()
+        model = UNet(3, 1, True)
+        detector = ImageManipulationDetector(model)
         if serializer.is_valid():
             image_file = serializer.validated_data['image']
+
+            validation_response = self.validate_image(image_file)
+            if validation_response is not None:
+                return validation_response
+
             image = Image.open(image_file)
 
             # detection
-            heatmap = detector.predict(image)
-            heatmap = (heatmap * 255).astype(np.uint8)
-            heatmap_pil = Image.fromarray(heatmap)
+            try:
+                heatmap = detector.predict(image)
+            except RuntimeError as e:
+                return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
 
             buffer = io.BytesIO()
-            heatmap_pil.save(buffer, format='PNG') # save the PIL image to the buffer in PNG format
+            heatmap.save(buffer, format='PNG') # save the PIL image to the buffer in PNG format
             byte_im = buffer.getvalue()
 
             return HttpResponse(byte_im, content_type='image/png')
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def validate_image(self, image_file):
+        # check file type
+        allowed_file_types = ['image/jpeg', 'image/png']
+        if image_file.content_type not in allowed_file_types:
+            return Response({'error': 'Unsupported file type. Allowed types are JPEG and PNG.'}, status=status.HTTP_400_BAD_REQUEST)
+        
+        # check file size (limit to 5MB)
+        max_file_size = 5 * 1024 * 1024  # 5 MB
+        if image_file.size > max_file_size:
+            return Response({'error': 'File size exceeds the limit of 5MB.'}, status=status.HTTP_400_BAD_REQUEST)
+        
+        # additional
+        try:
+            image = Image.open(image_file)
+            image.verify()
+            image = Image.open(image_file)
+            image = image.convert('RGB')
+        except (IOError, SyntaxError):
+            return Response({'error': 'Invalid image file.'}, status=status.HTTP_400_BAD_REQUEST)
+
+
+'''
+#####################
+# API Documentation #
+#####################
+
+Endpoint:
+POST /api/detect-regions
+
+Request Body:
+- Content-Type: multipart/form-data
+- Parameters:
+  - image: Image file (JPEG or PNG format, maximum size 5MB)
+
+Responses:
+- 200 OK:
+  - Returns a PNG image containing the heatmap visualization of detected regions.
+  - Content-Type: image/png
+  
+- 400 Bad Request:
+  - If the request body is invalid or missing required parameters.
+  - Error JSON example:
+    {
+      "error": "Unsupported file type. Allowed types are JPEG and PNG."
+    }
+
+- 500 Internal Server Error:
+  - If an unexpected error occurs during image processing or model prediction.
+  - Error JSON example:
+    {
+      "error": "Error during prediction: <error_message>"
+    }
+'''
--- model/detector.py
+++ model/detector.py
@@ -2,29 +2,71 @@ import torch
 import torchvision.transforms as transforms
 from PIL import Image
 import numpy as np
+from matplotlib.colors import LinearSegmentedColormap
+
+def define_colormap():
+    colors = [
+        (0.0, (0, 0, 0, 0)),
+        (0.1, (0, 0, 0, 0)),
+        (0.2, (0, 0, 0, 0)),
+        (0.3, (102/255, 205/255, 170/255, 0.0)),
+        (0.4, (102/255, 205/255, 170/255, 0.0)),
+        (0.5, (0/255, 0/255, 255/255, 0.5)),
+        (0.6, (102/255, 205/255, 170/255, 0.6)),
+        (0.65, (102/255, 205/255, 170/255, 0.7)),
+        (0.7, (255/255, 255/255, 0/255, 0.3)),       
+        (0.8, (255/255, 255/255, 0/255, 0.5)),
+        (1.0, 'red')
+    ]  
+    cmap_name = 'transparent_cyan_blue_yellow_red'
+    custom_cmap = LinearSegmentedColormap.from_list(cmap_name, colors)
+    return custom_cmap
 
 class ImageManipulationDetector:
-    def __init__(self, model_path='model/model.pth'):
-        self.device = torch.cuda
-        self.model = self._load_model(model_path)
+    def __init__(self, base_model, model_path='model/v1/best_model.pth'):
+        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        self.model = self._load_model(model_path, base_model)
         
         self.transform = transforms.Compose([
-            transforms.Resize((224, 224)),
+            transforms.Resize((256, 256)),
             transforms.ToTensor(),
         ])
         
-    def _load_model(self, model_path):
-        model = torch.load(model_path, map_location=self.device)
-        model.eval()
-        return model
+        self.color_map = define_colormap()
+        
+    def _load_model(self, model_path, base_model):
+        base_model.load_state_dict(torch.load(model_path, map_location=self.device))
+        base_model.to(self.device)
+        base_model.eval()
+        return base_model
 
     def predict(self, image):
-        image = self.transform(image)
-        image = image[None]
-        with torch.no_grad():
-            mask = self.model(image)
-        heatmap = self._get_heat_map(image, mask)
-        return heatmap
+        try:
+            image_tf = self.transform(image)
+            image_tf = image_tf[None]
+            image_tf = image_tf.to(self.device)
+            with torch.no_grad():
+                mask = self.model(image_tf)
+            heatmap = self._get_heat_map(image, mask)
+            return heatmap
+        except Exception as e:
+            raise RuntimeError(f'Error during prediction: {str(e)}')
+    
     
     def _get_heat_map(self, image, mask):
-        return np.zeros((2, 2))
+        original_size = image.size
+        # normalize mask
+        mask = np.squeeze(mask)
+        mask_min, mask_max = mask.min(), mask.max()
+        normalized_mask = (mask - mask_min) / (mask_max - mask_min)
+        
+        # get heatmap
+        heatmap = self.color_map(normalized_mask.cpu().numpy())
+        heatmap = (heatmap * 255).astype(np.uint8)
+        heatmap = Image.fromarray(heatmap)
+        unresized_heatmap = heatmap.resize(original_size, Image.BICUBIC)
+        
+        # overlay the heatmap on the input image
+        image.paste(unresized_heatmap, (0,0), mask = unresized_heatmap)
+
+        return image
